a9ac035b0ab21c7c0d12ba933ad86349acead6ff
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-08-24 13:43:16 +0200

    fixed_countPosition

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import Map from "./Components/Map";
 import Store from "./Components/Store";
 import "./Styles/App.scss"
+import RobotCounterContext from "./Contexts/robotCounterContext.jsx"
 
 
 export default function App(){
+
+    const [robotCounter,setRobotCounter] = useState(200)
+
+
     return <div className="gameWrapper">
-        <Map>
-        </Map>
-        <Store>
-        </Store>
+        <RobotCounterContext.Provider value ={[robotCounter,setRobotCounter]}>
+            <Map>
+            </Map>
+            <Store>
+            </Store>
+        </RobotCounterContext.Provider>
     </div>
 }
