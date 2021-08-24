c674b4d209e1009be09d312ef9a93fb6bd7abfc4
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-08-24 15:58:37 +0200

    some css

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,17 +1,21 @@
 import React, { useState,useContext } from "react"
 import "../Styles/Map.scss"
 import RobotGenerator from "./RobotGenerator"
+
 // import RobotCounter from "./RobotCounter"
 
+import imgCloud from "../resources/cloud.png"
 
 export default function Map(){
    
     return (
         <div className="Map">
-            
+
                 <RobotGenerator />
-                
-            
+                <img src={imgCloud} className="imgCloud" alt="" />
+                <img src="img_girl.jpg" alt="Girl in a jacket" />
+                <img src="img_girl.jpg" alt="Girl in a jacket" />
+            <div className="Planet"/>
         </div>
     )
     
