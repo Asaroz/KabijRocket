8c84e6a6652eb3604644581f6586d55a802d56e3
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-08-24 14:57:03 +0200

    GameTik added

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -2,20 +2,16 @@ import React, { useState,useContext } from "react"
 import "../Styles/Map.scss"
 import RobotGenerator from "./RobotGenerator"
 // import RobotCounter from "./RobotCounter"
-import RobotCounterContext from "../Contexts/robotCounterContext.jsx"
+
 
 export default function Map(){
    
-
-    const [robotCounter,setRobotCounter] = useState(200)
-
-
     return (
         <div className="Map">
-            <RobotCounterContext.Provider value ={[robotCounter,setRobotCounter]}>
+            
                 <RobotGenerator />
                 
-            </RobotCounterContext.Provider>
+            
         </div>
     )
     
