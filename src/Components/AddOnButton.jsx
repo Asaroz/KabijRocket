fe8c991ca1b2806bf34811ab86399c8988bc196a
mariagluck <mariamart.berlin@gmail.com>
2021-08-25 10:10:10 +0200

    addons and tests

--- src/Components/AddOnButton.jsx
+++ src/Components/AddOnButton.jsx
@@ -1,48 +1,100 @@
 import React, { createContext, useContext, useReducer } from 'react';
+import robotCounterContext from '../Contexts/robotCounterContext.jsx';
 import RobotCounterContext from "../Contexts/robotCounterContext.jsx";
+import RobotPerSecondContext from "../Contexts/robotPerSecondContext.jsx";
 
-const [ robotCounter, setRobotCounter ] = useContext(RobotCounterContext);
 
-const initialState = {
-    robotsPerSec: 4,
-    imagePrinted: null, 
-    amount: 2,
+
+export default  function AddOnButton() {
+    const [ robotCounter, setRobotCounter ] = useContext(RobotCounterContext);
+    const [ rpsCounter, setRpsCounter ] = useContext(RobotPerSecondContext);
     
+    //  rpsCounter = rpsCounter + amount *robotspersecond
+    const initialState = {
+         robotsPerSec: 4,
+         imagePrinted: null, 
+         amount: 2,
+         count: 0,
+         cost: null,
+    
+       };
+      
+     const reducer = (state, action) => {
+         switch (action.type) {
+           case 'WATER_INCREMENT':
+             return {
+                robotsPerSec: 4,
+                imagePrinted: null, 
+                amount: 2,
+                count: 0,
+                cost: null,
+             };
+      
+     
+           case 'VEGETATION_INCREMENT':
+             return {
+              count: state.count + 20
+             };
+           
+        
+
+           case 'HOUSING_INCREMENT':
+               return {
+                count: state.count + 20
+            };
 
 
-  };
- 
-const reducer = (state, action) => {
-    switch (action.type) {
-      case 'WATER_INCREMENT':
-        return {
-            count: state.count + 1
-        };
- 
-
-      case 'VEGETATION_INCREMENT':
-        return {
-         count: state.count + 20
-        };
-      default:
-        return state;
-    }
-  };  
-
-
-
-
-  const Store = () => {
+           case 'DINOS_INCREMENT':
+           return {
+            count: state.count + 20
+           };
+              
+           
+  
+           case 'ENERGY_INCREMENT':
+               return {
+                count: state.count + 20
+               };
+            
+           
+  
+           case 'FACTORY_INCREMENT':
+               return {
+                count: state.count + 20
+               };
+             
+           
+  
+           case 'FARMING_INCREMENT':
+               return {
+                count: state.count + 20
+               };
+           
+           
+  
+           case 'MINING_INCREMENT':
+               return {
+                count: state.count + 20
+               };
+             default:
+             return state;
+        }
+       };  
     const [state, dispatch] = useReducer(reducer, initialState);
     return (
       <div>
          <h1> Count: {state.count}</h1>
-        <button onClick={() => dispatch({type: 'WATER_INCREMENT'})}><h2>WATER</h2></button>
-        <button onClick={() => dispatch({type: 'VEGETATION_INCREMENT'})}><h2>VEGETATION</h2></button>
+        <button onClick={() => dispatch({type: 'WATER_INCREMENT'})} className="water btn"><h2>WATER</h2></button>
+        <button onClick={() => dispatch({type: 'VEGETATION_INCREMENT'})} className="veg btn"><h2>VEGETATION</h2></button>
+        <button onClick={() => dispatch({type: 'HOUSING_INCREMENT'})} className="housing btn"><h2>HOUSING</h2></button>
+        <button onClick={() => dispatch({type: 'DINOS_INCREMENT'})} className="dinos btn"><h2>DINOS</h2></button>
+        <button onClick={() => dispatch({type: 'ENERGY_INCREMENT'})} className="energy btn"><h2>ENERGY</h2></button>
+        <button onClick={() => dispatch({type: 'FACTORY_INCREMENT'})} className="factory btn"><h2>FACTORY</h2></button>
+        <button onClick={() => dispatch({type: 'FARMING_INCREMENT'})} className="farm btn"><h2>FARMING</h2></button>
+        <button onClick={() => dispatch({type: 'MINING_INCREMENT'})} className="mine btn"><h2>MINING</h2></button>
        
       </div>
     );
   };
   
 
-export default Store;
--- src/Contexts/robotCounterContext.jsx
+++ src/Contexts/robotCounterContext.jsx
@@ -1,5 +1,5 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 
-const robotCounterContext = React.createContext(0)
+const robotCounterContext = React.createContext()
 
 export default robotCounterContext
