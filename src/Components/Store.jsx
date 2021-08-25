f17e342440dc79120f3a2b9327d4428cc19a81f1
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-08-25 15:35:23 +0200

    new base

--- src/Components/Store.jsx
+++ src/Components/Store.jsx
@@ -2,21 +2,19 @@ import "../Styles/Store.scss";
 import AddOnButton from "./AddOnButton";
 import RobotCounterContext from "../Contexts/robotCounterContext.jsx"
 import React, { useState, useContext } from "react"
-import RobotGenerator from "./RobotGenerator"
+
 
 
 export default function Store(){
    
 
-    const [robotCounter,setRobotCounter] = useState(200)
+
 
 
     return (
         <div className="Store">
-            <RobotCounterContext.Provider value ={[robotCounter,setRobotCounter]}>
                 <AddOnButton />
-                
-            </RobotCounterContext.Provider>
+
         </div>
     )
     
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import ReactDOM  from "react-dom";
 import App from "./App.jsx"
+import AddonContextProvider from "./Contexts/AddonObjContext.jsx";
+
 
 ReactDOM.render(
-    <div>
-        <App></App>
-    </div>
+        <AddonContextProvider>
+                 <App></App>
+        </AddonContextProvider> 
 ,
 document.getElementById("root")
 )
