729850fe359bbbe210ad1ce688e8f8e99d72c656
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-08-26 16:00:44 +0200

    new base

--- src/Components/AddOnButton.jsx
+++ src/Components/AddOnButton.jsx
@@ -38,6 +38,7 @@ export default  function AddOnButton() {
 
        
     return (
+<<<<<<< Updated upstream
       <div className="btnContainer">
         
         <div onClick={() => Click({type: 'WATER_INCREMENT'},state.water)} className="water btn"><img className="addOnicon" src={waterButton}/><h2>WATER</h2><h3>{state.water.amount}</h3></div>
@@ -48,9 +49,22 @@ export default  function AddOnButton() {
         <div onClick={() => Click({type: 'FACTORY_INCREMENT'},state.factory)} className="factory btn"><img className="addOnicon" src={factoryButton}/><h2>FACTORY</h2><h3>{state.factory.amount}</h3></div>
         <div onClick={() => Click({type: 'FARMING_INCREMENT'},state.farming)} className="farm btn"><img className="addOnicon" src={farmingButton}/><h2>FARMING</h2><h3>{state.farming.amount}</h3></div>
         <div onClick={() => Click({type: 'MINING_INCREMENT'},state.mining)} className="mine btn"><img className="addOnicon" src={miningButton}/><h2>MINING</h2><h3>{state.mining.amount}</h3></div>
+=======
+      <div className="buttons">
+        
+        <button className="addOnBtn" onClick={() => Click({type: 'WATER_INCREMENT'},state.water)} className="water btn"><h2 className="addOnName">WATER</h2>{state.water.amount}</button>
+        <button className="addOnBtn" onClick={() => Click({type: 'VEGETATION_INCREMENT'},state.veg)} className="veg btn"><h2 className="addOnName">VEGETATION</h2>{state.veg.amount}</button>
+        <button className="addOnBtn" onClick={() => Click({type: 'HOUSING_INCREMENT'},state.house)} className="housing btn"><h2 className="addOnName">HOUSING</h2>{state.house.amount}</button>
+        <button className="addOnBtn" onClick={() => Click({type: 'DINOS_INCREMENT'},state.dinos)} className="dinos btn"><img className="addOnicon" src={dinoButton}/><h2>DINOS</h2>{state.dinos.amount}</button>
+        <button className="addOnBtn" onClick={() => Click({type: 'ENERGY_INCREMENT'},state.energy)} className="energy btn"><h2 className="addOnName">ENERGY</h2>{state.energy.amount}</button>
+        <button className="addOnBtn" onClick={() => Click({type: 'FACTORY_INCREMENT'},state.factory)} className="factory btn"><h2 className="addOnName">FACTORY</h2>{state.factory.amount}</button>
+        <button className="addOnBtn" onClick={() => Click({type: 'FARMING_INCREMENT'},state.farming)} className="farm btn"><img className="addOnicon" src={farmingButton}/><h2 className="addOnName">FARMING</h2>{state.farming.amount}</button>
+        <button className="addOnBtn" onClick={() => Click({type: 'MINING_INCREMENT'},state.mining)} className="mine btn"><h2>MINING</h2>{state.mining.amount}</button>
+>>>>>>> Stashed changes
        
       </div>
     );
   };
   
 
+                                                                                       
