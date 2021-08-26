b630c0e23fbc287be95c88944a9cbf22a68ec9f7
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-08-26 11:40:34 +0200

    new base

--- src/Components/AddOnButton.jsx
+++ src/Components/AddOnButton.jsx
@@ -2,7 +2,8 @@ import React, { createContext, useContext, useReducer, useState } from 'react';
 import RobotCounterContext from "../Contexts/robotCounterContext.jsx";
 import RobotPerSecondContext from "../Contexts/robotPerSecondContext.jsx";
 import {addonObjContext} from "../Contexts/AddonObjContext.jsx";
-
+import dinoButton from "../resources/dinosaur-button.png"
+import farmingButton from "../resources/farming-button.png"
 
 
 export default  function AddOnButton() {
@@ -29,10 +30,10 @@ export default  function AddOnButton() {
         <button onClick={() => Click({type: 'WATER_INCREMENT'},state.water)} className="water btn"><h2>WATER</h2>{state.water.amount}</button>
         <button onClick={() => Click({type: 'VEGETATION_INCREMENT'},state.veg)} className="veg btn"><h2>VEGETATION</h2>{state.veg.amount}</button>
         <button onClick={() => Click({type: 'HOUSING_INCREMENT'},state.house)} className="housing btn"><h2>HOUSING</h2>{state.house.amount}</button>
-        <button onClick={() => Click({type: 'DINOS_INCREMENT'},state.dinos)} className="dinos btn"><h2>DINOS</h2>{state.dinos.amount}</button>
+        <button onClick={() => Click({type: 'DINOS_INCREMENT'},state.dinos)} className="dinos btn"><h2>DINOS</h2><img className="addOnicon" src={dinoButton}/>{state.dinos.amount}</button>
         <button onClick={() => Click({type: 'ENERGY_INCREMENT'},state.energy)} className="energy btn"><h2>ENERGY</h2>{state.energy.amount}</button>
         <button onClick={() => Click({type: 'FACTORY_INCREMENT'},state.factory)} className="factory btn"><h2>FACTORY</h2>{state.factory.amount}</button>
-        <button onClick={() => Click({type: 'FARMING_INCREMENT'},state.farming)} className="farm btn"><h2>FARMING</h2>{state.farming.amount}</button>
+        <button onClick={() => Click({type: 'FARMING_INCREMENT'},state.farming)} className="farm btn"><h2>FARMING</h2><img className="addOnicon" src={farmingButton}/>{state.farming.amount}</button>
         <button onClick={() => Click({type: 'MINING_INCREMENT'},state.mining)} className="mine btn"><h2>MINING</h2>{state.mining.amount}</button>
        
       </div>
